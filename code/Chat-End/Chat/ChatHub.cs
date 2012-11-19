89bbf0a18586449259ffc3f7f0cfa28d23ad16e5
brady gaster <brady.gaster@gmail.com>
2012-11-19 12:11:09 -0800

    added chat demo, which will be an introduction demo

---
+++ code/Chat-End/Chat/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace Chat
+{
+    [HubName("chat")]
+    public class ChatHub : Hub
+    {
+        public void SendMessage(dynamic message)
+        {
+            Clients.All.onReceiveMessage(message);
+        }
+    }
+}
