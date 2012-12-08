e4cd644babd32ba2c95c5ee79ff02bcd10fd407c
bradygaster <brady.gaster@gmail.com>
2012-12-07 16:05:10 -0800

    added hitcounter demo and demo script

---
+++ source/HitCounter-End/HitCounter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SignalRHitCount
+{
+    public class HitCounter : Hub
+    {
+        static int _hitCount;
+
+        public void RecordHit()
+        {
+            _hitCount += 1;
+            Clients.All.onHitRecorded(_hitCount);
+        }
+
+        public override System.Threading.Tasks.Task OnDisconnected()
+        {
+            _hitCount -= 1;
+            Clients.All.onHitRecorded(_hitCount);
+
+            return base.OnDisconnected();
+        }
+    }
+}
